bad6d7561ecd264ad7f5e4ff8e102bf0f919c672
Maryam Awan <maryamawan04@gmail.com>
2023-01-12 10:47:31 +0000

    Linear Search

---
+++ Data_Structures_and_Algorithms/linear_search.py
@@ -0,0 +1,10 @@
+item = 18
+
+numbers = [2, 54, 60, 7, 19, 43, 18, 75, 30]
+
+for number in numbers:
+    if item == number:
+        print("Item Found!")
+        break
+    else:
+        continue
